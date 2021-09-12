ecea04842453aa48aaa697388a2bfb97493a9dbf
Joe Chasinga <jo.chasinga@gmail.com>
2021-09-11 22:11:51 -0700

    Refactor

--- src/flow/transactions/pets/MintPetToken.tx.ts
+++ src/flow/transactions/pets/MintPetToken.tx.ts
@@ -2,42 +2,26 @@ import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types";
 import Pet, {petToCadenceDict} from "pet";
 import { NFTStorage, File } from "nft.storage";
-import data from "pets.json";
 import cadenceScript from "flow/transactions/pets/MintPetToken.cdc";
 import {authz} from "./authz";
 
 const client = new NFTStorage({
-    // token: process.env.REACT_APP_NFTSTORAGE_API_KEY!
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDM5YzIyMUUzOTFiNDMwMzQ4NDc2NzdmMmVGZTc1ODRGNTM2ZjM4OWEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzMDQ0MDM0Nzg2NiwibmFtZSI6IlBldHNob3AifQ.5i8IsZX9zbaqGFfVjhwZw6VX3IPZKQ5Xr4BLMYAM9jo"
+    token: process.env.REACT_APP_NFTSTORAGE_API_KEY!
 });
 
 const uploadToStorage = async (pet: Pet) => {
-    console.log('uploading ', pet.photo);
     if (pet.photo) {
         const url = await fetch(pet.photo);
         const data = await url.blob();
-        // const uri = client.storeBlob(data);
-        const metadata =  await client.store({
-            name:  pet.name,
-            description: `Photo of ${pet.name}`,
-            image: new File([data], `${pet.name}.jpg`, { type: "image/jpg" })
-        });
+        const metadata = await client.store({
+            ...pet,
+            description: `Data of ${pet.name}`,
+            image: new File([data], `${pet.name}.jpg`, { type: "image/jpg" }),
+        } as any);
         return metadata;
     }
+    return null;
 }
-
-/*
-const mintPetTokens = async () => {
-    const pets = data.pets;
-    pets.forEach(async (pet) => {
-        // const uri = await uploadToStorage(pet)
-        // pet.uri = uri;
-        console.log("pet: ", pet);
-        const res = await mintPetToken(pet);
-        console.log(res);
-    });
-}
-*/
 
 export const mintPetToken = async (pet: Pet) => {
     const code = await (await fetch(cadenceScript)).text();
@@ -58,14 +42,10 @@ export const mintPetToken = async (pet: Pet) => {
             payload,
         ])
         let txId = await fcl.decode(encoded);
-
         let petData: any = await uploadToStorage(pet);
-
         return { txId, image: { ...petData }};
-        // return fcl.tx(txId).onceSealed();
     } catch (err) {
-        console.error("ERRRR", err);
-        return;
+        console.error(err);
     }
 }
 
